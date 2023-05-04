8c608771e53bc70af20c2076b05ed584f423274b
michaelpginn <milespergallon271@gmail.com>
2023-05-03 20:47:21 -0600

    Revert to standard BERT params

--- src/taxonomic_loss_model.py
+++ src/taxonomic_loss_model.py
@@ -130,9 +130,9 @@ def create_model(encoder: MultiVocabularyEncoder, sequence_length) -> BertPreTra
     print("Creating model...")
     config = BertConfig(
         vocab_size=len(encoder.vocabularies[0]) + len(encoder.special_chars),
-        num_hidden_layers=8,
-        num_attention_heads=8,
-        hidden_size=512,
+        # num_hidden_layers=8,
+        # num_attention_heads=8,
+        # hidden_size=512,
         max_position_embeddings=sequence_length,
         pad_token_id=encoder.PAD_ID,
         num_labels=len(encoder.vocabularies[1])
