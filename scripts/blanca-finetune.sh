a4a74812f80b1d0577c7a762ab794b7bcc2cad65
michaelpginn <milespergallon271@gmail.com>
2023-05-07 18:13:56 -0600

    Update script for harmonic models

--- scripts/blanca-finetune.sh
+++ scripts/blanca-finetune.sh
@@ -24,7 +24,7 @@ do
   do
 #    for loss in tax tax_simple
 #    do
-    python3 finetune_token_classifier.py train flat --train_size $size --deeper_classifier true --seed $seed
+    python3 finetune_token_classifier.py train tax --train_size $size --loss_sum harmonic --seed $seed
 #    done
   done
 done
