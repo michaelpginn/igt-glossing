db13420a5352006d8b548f707bb2ad25248c148f
michaelpginn <milespergallon271@gmail.com>
2023-06-09 00:10:27 -0700

    Fix path

--- scripts/blanca-finetune.sh
+++ scripts/blanca-finetune.sh
@@ -16,7 +16,7 @@ module purge
 module load anaconda
 # Run Python Script
 conda activate AutoIGT
-cd "/projects/migi8081/struct-morph/src"
+cd "/projects/migi8081/taxo-morph/src"
 
 #for seed in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 #do
