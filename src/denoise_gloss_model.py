8789f9f752e5b7fedbf9463b4c59d12dcce1f2ac
michaelpginn <milespergallon271@gmail.com>
2023-08-24 14:39:40 -0500

    Change model name

--- src/denoise_gloss_model.py
+++ src/denoise_gloss_model.py
@@ -94,7 +94,7 @@ def train(arch_size: str = 'micro',
     print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
     print(eval_results)
 
-    trainer.save_model(f'../models/usp-lang-model')
+    trainer.save_model(f'../models/usp-gloss-denoiser')
 
 
 if __name__ == "__main__":
