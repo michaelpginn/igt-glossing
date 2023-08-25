beab82bc0c49ca44ab879f1a7948713030f631e0
michaelpginn <milespergallon271@gmail.com>
2023-08-25 17:41:50 -0500

    Change train set

--- scripts/blanca-genbench-pretrain-lm.sh
+++ scripts/blanca-genbench-pretrain-lm.sh
@@ -17,4 +17,4 @@ module load anaconda
 # Run Python Script
 conda activate AutoIGT
 cd "/projects/migi8081/taxo-morph/src"
-python3 pretrain_language_model.py --arch_size micro --project genbench-taxo-morph-pretrain --train_data ../data/GenBench/train --eval_data ../data/GenBench/eval_ID --position_embeddings absolute
+python3 pretrain_language_model.py --arch_size micro --project genbench-taxo-morph-pretrain --train_data ../data/GenBench/story --eval_data ../data/GenBench/personal --position_embeddings absolute
