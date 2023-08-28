596315433aefc8b58926abc9497bc7ec17911fb6
michaelpginn <milespergallon271@gmail.com>
2023-08-27 19:18:21 -0600

    Update exp 1

--- scripts/blanca-genbench-pretrain-lm.sh
+++ scripts/blanca-genbench-pretrain-lm.sh
@@ -17,5 +17,5 @@ module load anaconda
 # Run Python Script
 conda activate AutoIGT
 cd "/projects/migi8081/taxo-morph/src"
-python3 pretrain_language_model.py --arch_size micro --project genbench-taxo-morph-pretrain --train_data ../data/GenBench/train.txt --eval_data ../data/GenBench/eval_id --position_embeddings absolute
+python3 pretrain_language_model.py --arch_size micro --project genbench-taxo-morph-pretrain --train_data ../data/GenBench/train.txt --eval_data ../data/GenBench/eval_id.txt --position_embeddings absolute
 #python3 pretrain_language_model.py --arch_size full --project genbench-taxo-morph-pretrain --train_data ../data/GenBench/train --eval_data ../data/GenBench/eval_ID --position_embeddings relative_key_query
